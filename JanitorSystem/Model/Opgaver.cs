8becad2f7193e70a9926f8bd2107cdaf04e56e5d
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-03-31 11:25:37 +0200

    Properties tilføjet til 'Opgaver'.

--- JanitorSystem/Model/Opgaver.cs
+++ JanitorSystem/Model/Opgaver.cs
@@ -8,5 +8,11 @@ namespace JanitorSystem.Model
 {
     class Opgaver
     {
+        public string Adresse { get; set; }
+        public string AnmelderFornavn { get; set; }
+        public string AnmelderEfternavn { get; set; }
+        public string OpgaveBeskrivelse { get; set; }
+        public int Opgave_Id { get; set; }
+        public string Titel { get; set; }
     }
 }
