74ce970088ed7f5763e64a07301b7fe6cd16f018
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-04-24 11:07:47 +0200

    Lavet properties til 'Employee' klassen. Har lavet en constructor og en override af ToString().

--- JanitorSystem/Model/Employee.cs
+++ JanitorSystem/Model/Employee.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace JanitorSystem.Model
 {
-    class Employee
+    public class Employee
     {
+        #region Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public int WorkHours { get; set; }
+        public int Phone { get; set; }
+        public string Title { get; set; }
+        #endregion
+
+        // ctor
+        public Employee()
+        {
+
+        }
+        public override Tostring()
+        {
+
+        }
     }
 }
