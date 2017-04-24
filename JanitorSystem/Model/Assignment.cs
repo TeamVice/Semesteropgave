4abd2d255d6235b0af68cfe903dba385361476d0
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-04-24 12:40:53 +0200

    Har tilføjet properties til 'Assignemt'-klassen

--- JanitorSystem/Model/Assignment.cs
+++ JanitorSystem/Model/Assignment.cs
@@ -8,5 +8,21 @@ namespace JanitorSystem.Model
 {
     class Assignment
     {
+        public string clientFirstName { get; set; }
+        public string clientLastName { get; set; }
+        public string clientAddress { get; set; }
+        public int assignId { get; set; }
+        public string assignTitle { get; set; }
+        public string assignDescription { get; set; }
+
+        public Assignment()
+        {
+
+        }
+
+        //public override string ToString()
+        //{
+        //    return null;
+        //}
     }
 }
