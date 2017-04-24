a2259ae44e9b0bb193e2c1386054c9141273d067
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-04-24 12:46:08 +0200

    Har implementeret interfacet 'INotifyPropertyChanged' til klassen 'ViewModel'.

--- JanitorSystem/ViewModel/MainViewModel.cs
+++ JanitorSystem/ViewModel/MainViewModel.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using JanitorSystem.Model;
 
 namespace JanitorSystem.ViewModel
 {
-    class MainViewModel
+    class MainViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
