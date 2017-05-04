877fc316f46493733876d738bea1b0b8dc88336a
Kasper <rtskasper@gmail.com>
2017-05-04 10:35:31 +0200

    Har rettet xaml kode i forhold til klassens navn fra AssignmentList til AssignmentListFrontPage + rettet app xaml start navigation til AssigmentListFrontPage

--- JanitorSystem/View/AssignmentListFrontPage.xaml.cs
+++ JanitorSystem/View/AssignmentListFrontPage.xaml.cs
@@ -20,9 +20,9 @@ namespace JanitorSystem.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class AssignmentList : Page
+    public sealed partial class AssignmentListFrontPage : Page
     {
-        public AssignmentList()
+        public AssignmentListFrontPage()
         {
             this.InitializeComponent();
         }
