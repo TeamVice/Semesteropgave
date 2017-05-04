499b8075aab3a38d3978b8d6895163a03557d90d
Kasper <rtskasper@gmail.com>
2017-05-04 12:09:00 +0200

    Indsat server url i facade klassen som en konstant string. Installeret Nugget pakker WebAPI.Client og NewtonSoft Json i Janitor solution.

--- JanitorSystem/Facade/FacadeService.cs
+++ JanitorSystem/Facade/FacadeService.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JanitorSystem.Model;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace JanitorSystem.Facade
 {
     public class FacadeService
     {
+        private const string serverUrl = "http://vicewebservices20170502020205.azurewebsites.net";
+
+        //public static async 
+
     }
 }
