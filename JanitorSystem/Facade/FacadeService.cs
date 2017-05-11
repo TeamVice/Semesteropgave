d5d7f25b8e4ea279a93c702ef9b425d61ef5a672
Kasper <rtskasper@gmail.com>
2017-05-11 11:39:28 +0200

    Opdateret database.

--- JanitorSystem/Facade/FacadeService.cs
+++ JanitorSystem/Facade/FacadeService.cs
@@ -16,7 +16,7 @@ namespace JanitorSystem.Facade
 {
     public class FacadeService
     {
-        private const string serverUrl = "http://vicewebservice20170509030408.azurewebsites.net";
+        private const string serverUrl = "http://vicewebservice20170511113133.azurewebsites.net";
 
         #region Get Http kald
 
