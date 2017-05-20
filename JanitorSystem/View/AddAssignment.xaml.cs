467a7b3c0c89f6d75901651e1a1d28b5b1324f0b
Kasper <rtskasper@gmail.com>
2017-05-20 12:26:22 +0200

    Stretchet button i xaml koden under combobox priotering. og lavet en click funktion der sætter placeholdertext

--- JanitorSystem/View/AddAssignment.xaml.cs
+++ JanitorSystem/View/AddAssignment.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using JanitorSystem.Model;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -35,6 +36,27 @@ namespace JanitorSystem.View
             this.Frame.Navigate(typeof(LogInPage), null);
         }
 
+        private void ClosePriorityOne(object sender, RoutedEventArgs e)
+        {
+            
+            AssignRankNoComboBox.IsDropDownOpen = false;
+            AssignRankNoComboBox.PlaceholderText = "Akut";
 
+
+
+        }
+
+        private void ClosePriorityTwo(object sender, RoutedEventArgs e)
+        {
+            AssignRankNoComboBox.IsDropDownOpen = false;
+            AssignRankNoComboBox.PlaceholderText = "Alm opgave";
+        }
+
+        private void ClosePriorityThree(object sender, RoutedEventArgs e)
+        {
+            AssignRankNoComboBox.IsDropDownOpen = false;
+            AssignRankNoComboBox.PlaceholderText = "Kan vente";
+
+        }
     }
 }
